51dc5f6ddd4ad6fb9f537438030554424c5aceef
cycdc98 <cycdc98@163.com>
2026-02-13 09:14:46 +0800

    更新时间+搜索

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -25,6 +25,39 @@ export default defineConfig({
     socialLinks: [
       { icon: "github", link: "https://github.com/cycdc98/cycdc98.github.io" },
     ],
+    lastUpdated: {
+      text: "更新时间",
+    },
+    search: {
+      provider: "local",
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: "搜索",
+                buttonAriaLabel: "搜索",
+              },
+              modal: {
+                displayDetails: "显示详细列表",
+                resetButtonTitle: "重置搜索",
+                backButtonTitle: "关闭搜索",
+                noResultsText: "没有结果",
+                footer: {
+                  selectText: "选择",
+                  selectKeyAriaLabel: "输入",
+                  navigateText: "导航",
+                  navigateUpKeyAriaLabel: "上箭头",
+                  navigateDownKeyAriaLabel: "下箭头",
+                  closeText: "关闭",
+                  closeKeyAriaLabel: "Esc",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
   srcDir: "docs",
   vite: {
